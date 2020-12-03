56f06d0502e4499ccfdb1fcfcd8f2a6490e49a59
Vinay P <vinayp@pop-os.localdomain>
2020-12-04 02:16:04 +0530

    eventlistener set to state hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
-import * as React from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import "./App.css";
 import Board from "./components/board";
 import { defaultBoardConfig } from "./core/constants";
+import * as types from "./core/types";
 
 export default function App() {
-  const [configJson, setConfigJson] = React.useState(defaultBoardConfig);
-  window.addEventListener("message", (event) => {
+  const [configJson, _setConfigJson] = useState(defaultBoardConfig);
+  const configRef = useRef<types.Board>(configJson);
+
+  const setConfigJson = (data: types.Board) => {
+    configRef.current = data;
+    _setConfigJson(configRef.current);
+  };
+
+  const handleMsgEvent = useCallback((event: any) => {
     const message = event.data;
-    switch (message.command) {
-      case "configJson":
-        setConfigJson(message.data);
-        break;
+    if (message.command && message.data) {
+      switch (message.command) {
+        case "configJson":
+          setConfigJson(message.data);
+          break;
+      }
     }
-  });
+  }, []);
+  useEffect(() => {
+    window.addEventListener("message", handleMsgEvent);
+    // clean up
+    return () =>
+      window.removeEventListener("message", (event) => handleMsgEvent);
+  }, [handleMsgEvent]);
 
   return (
     <>
